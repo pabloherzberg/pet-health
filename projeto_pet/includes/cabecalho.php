5dd911bd072c6e19ec9f482bb8cadcc7d4fb0854
Pablo Herzberg <pabloherzberg@gmail.com>
2019-04-03 11:54:18 -0300

    estilização index

--- projeto_pet/includes/cabecalho.php
+++ projeto_pet/includes/cabecalho.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="assets/js/jquery/index.js"></script>
--- projeto_pet/index.php
+++ projeto_pet/index.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="assets/css/index.css">
     <title>Document</title>
 </head>
-<body class="corpo">
-    <header class="header">
+<body>
+    <header>
         <figure>
             <img id="logo" src="assets/img/logo.png" alt="logo da empresa">
         </figure>
     </header>
-    <section class="form">
+    <main>
         <form action="includes/query.php" method="post">
             <div id="tela_selecionar" class="hide" >
                     <div class="btn" value='dono' id="btn_dono_pet">
@@ -24,30 +24,18 @@
                     </div>
                 </div>
             <div id="campos_preencher">
-                <input type="text" class='hide campo' name="nome" placeholder="nome">
-                <input class="campo" type="text" name="email" placeholder="emaill">
-                <input class="campo" type="text" name="senha" placeholder="senha">
-                <input type="text" class='hide campo' name="endereco" placeholder="endereço">
-                <input type="text" class='hide campo' name="telefone" placeholder="telefone">
-                <input class="botao_login" type="submit" id='acessar' name='acessar' value="login">
-                <input class="botao_cadastrar" type="button" id="cadastrar" value="Cadastrar">
-                <input type="submit" id='enviar' class="hide botao_enviar" name='enviar' value="Enviar">
+                <input type="text" class='hide' name="nome" placeholder="nome">
+                <input type="text" name="email" placeholder="emaill">
+                <input type="text" name="senha" placeholder="senha">
+                <input type="text" class='hide' name="endereco" placeholder="endereço">
+                <input type="text" class='hide' name="telefone" placeholder="telefone">
+                <input type="submit" class="btn" id='acessar' name='acessar' value="login">
+                <input type="button" class="btn" id="cadastrar" value="Cadastrar">
+                <input type="submit"class="hide btn" id='enviar' name='enviar' value="Enviar">
             </div>
         </form>
-    </section>
-    <footer class="footer">
-        <figure>
-            <img class="email rodape" src="assets/img/email.png" alt="ícone de email">
-        </figure>
-        <figure>
-            <img class="instagram rodape" src="assets/img/instagram.png" alt="ícone de instagram">
-        </figure>
-         <figure>
-            <img class="facebook rodape" src="assets/img/facebook.png" alt="ícone do facebook">
-         </figure>
-         <figure>
-         <img class="twitter rodape" src="assets/img/twitter.png" alt="ícone de twitter">
-         </figure>   
+    </main>
+    <footer>   
     </footer>
 </body>
 </html>
