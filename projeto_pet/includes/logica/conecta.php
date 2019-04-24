d9e4ec7b46228a638c28ab524d5f0ced7725a0b3
Pablo Herzberg <pabloherzberg@gmail.com>
2019-04-24 08:46:37 -0300

    pdo: conectar

--- projeto_pet/includes/logica/conecta.php
+++ projeto_pet/includes/logica/conecta.php
@@ -1,3 +1,4 @@
 <?php
-    $conexao = pg_connect("host=localhost port=5432 dbname=banco_pet user=postgres password=senha5");
+    $pdo = new PDO("pgsql:host=localhost; port=5432; dbname=banco_pet; user=postgres; password=senha5");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
