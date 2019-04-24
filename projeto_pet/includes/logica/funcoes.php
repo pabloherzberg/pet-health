491d90076ac6f52a721771be9363b25f41435b35
Pablo Herzberg <pabloherzberg@gmail.com>
2019-04-24 08:45:53 -0300

    pdo : inserirUsuário

--- projeto_pet/includes/logica/funcoes.php
+++ projeto_pet/includes/logica/funcoes.php
@@ -1,12 +1,14 @@
 <?php
-    function inserirUsuario($conexao, $array){
+    function inserirUsuario($array){
         if(count($array)==6){
-            $query = "insert into usuario(nome, email, senha, endereco, telefone, crmv) values ('$array[0]', '$array[1]', '$array[2]', '$array[3]', '$array[4]', '$array[5]')";
+            $stmt = $pdo->prepare("insert into usuario(nome, email, senha, endereco, telefone, crmv) values (?, ?, ?, ?, ?, ?)");
+            $query = $stmt->execute($array);
         }
         else{
-            $query = "insert into usuario(nome, email, senha, endereco, telefone) values ('$array[0]', '$array[1]', '$array[2]', '$array[3]', '$array[4]')";
+            $stmt = $pdo->prepare("insert into usuario(nome, email, senha, endereco, telefone) values (?, ?, ?, ?, ?)");
+            $query = $stmt->execute($array);
         }
-        return pg_query($conexao,$query);
+        return $query;
         /*Se o array possuir 6 posições é pq inclui o CRMV, logo o usuário é um VETERINÁRIO*/
     }
 
