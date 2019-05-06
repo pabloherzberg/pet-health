a313bc0dca7476475e837f7878c9afddd7017556
Pablo Herzberg <pabloherzberg@gmail.com>
2019-05-05 21:02:44 -0300

    ajustando os detalhes do footer

--- projeto_pet/includes/componentes/footer.php
+++ projeto_pet/includes/componentes/footer.php
@@ -1,3 +1,15 @@
-<footer>
-        <p>Patrocinadores:</p>
+<footer id='footer'>
+        <figure id='footerFigure'>
+                <img src="assets/img/pet.png">
+        </figure>
+        <h4>Pet&Health</h4>
+        <a id='contato'>Contato</a>
+        <a id='sobre'>Sobre</a>
+        <figure id='facebook'>
+                <img src="assets/img/facebook.png">
+        </figure>
+        <figure id='instagram'>
+                <img src="assets/img/instagram.png">
+        </figure>
+        <p>® 2019 Pet&Health</p>
 </footer>
