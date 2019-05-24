ff9339ec459bab7875677cacdb27b71518db0504
Pablo Herzberg <pabloherzberg@gmail.com>
2019-05-24 17:13:35 -0300

    home feita

---
+++ projeto_pet/home.php
@@ -0,0 +1,23 @@
+<?php require('includes/componentes/cabecalho.php');
+?>
+<link rel="stylesheet" href="assets/css/home.css">
+<script src="assets/js/home.js"></script>
+<title>Pet&Health</title>
+</head>
+<body>
+<?php require('includes/componentes/header.php') ?>
+<main>
+   <?php
+        $email = $_SESSION['email'];
+        if(isset($email)){
+            ?>
+                <section>
+                    <h2>bem vindo, <?php echo($_SESSION['nome']);?></h2>
+                </section>
+            <?php
+        }
+    ?>
+</main>
+<?php require('includes/componentes/footer.php');?>
+</body>
+</html>
