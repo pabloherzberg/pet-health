12b7fc862d6315b9242a68c87f0a64cc7963b24e
Gabriela Poncet <gabiponcet@gmail.com>
2019-05-24 17:42:38 -0300

    alteração em listarPets e addPet

--- projeto_pet/listarPets.php
+++ projeto_pet/listarPets.php
@@ -19,9 +19,33 @@
             <?php
         }
         foreach($pets as $pet){
+            $dt_nasc = $pet['dt_nascimento'];
+            $idade = idadePet($dt_nasc);
             ?>
                 <section>
-                    <h2><?php echo($pet['nome_pet']);?></h2>
+     <h2>Nome: <?php echo $pet['nome_pet']; ?></h2>
+     <p><?php echo $idade; ?> anos</p>
+     <p>Nascimento: <?php echo $pet['dt_nascimento']; ?></p>
+     <p>Raça: <?php echo $pet['raca']; ?></p>
+     <form action="includes/logica/logica.php" method="post">
+        <input type="hidden" name="cod_pet" value="<?=$pet['cod_pet']?>" />
+        <input type="hidden" name="nome_pet" value="<?=$pet['nome_pet']?>" />
+        <input type="hidden" name="dt_nascimento" value="<?=$pet['dt_nascimento']?>" />
+        <input type="hidden" name="raca" value="<?=$pet['raca']?>" />
+    </form>
+    <form action="historico.php" method="post">
+        <input type="hidden" name="cod_pet" value="<?=$pet['cod_pet']?>" />
+        <input type="submit" class="btn cadastro" name="verHistorico" value="Visualizar histórico"/>
+    </form> 
+       
+    <form action="alteraPet.php" method="post">
+        <input type="hidden" name="cod_pet" value="<?=$pet['cod_pet']?>" />
+        <input type="submit" class="btn cadastro" name="atualizaPet" value="Atualizar Cadastro Pet"/>
+    </form>
+     <form action="includes/logica/logica.php" method="post">
+        <input type="hidden" name="cod_pet" value="<?=$pet['cod_pet']?>" />
+        <input type="submit" class="btn cadastro" name="removerPet" value="Remover Pet"/>
+    </form>
                 </section>
             <?php
         }
