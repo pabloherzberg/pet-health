be7ffc22c47e8da802594ac7407869f595495d70
Pablo Herzberg <pabloherzberg@gmail.com>
2019-05-27 15:12:09 -0300

    reorganização da página addPet

--- projeto_pet/addPet.php
+++ projeto_pet/addPet.php
@@ -3,21 +3,21 @@ include_once('includes/componentes/cabecalho.php');
 include_once('includes/logica/conecta.php');
 ?>
   <link rel="stylesheet" href="assets/css/index.css">
-  <script src="assets/js/addPet.js"></script>
-    <title>Pet</title>
+    <title>addPet</title>
 </head>
 <body>
 <?php
     include_once("includes/componentes/header.php");
 ?>
 <main>
-<nav>
-<a href="home.php">Home</a>
-</nav>
-<button id='botao'  onclick='criarFormPet()'>Adicionar Pet</button>
-<div id="formulario">
-</div>  
-
+  <section>
+    <form action="includes/logica/logica.php" method="post">
+      <input type="text" name='nome' placeholder='nome'>
+      <input type="text" name='raca' placeholder='raça'>
+      <input type="date" name='dt_nascimento' placeholder='idade'>
+      <input type="submit" value='cadastrar' name='inserirPet'>
+    </form>
+  </section>
 </main>
 <?php require('includes/componentes/footer.php');?>
 </body>
